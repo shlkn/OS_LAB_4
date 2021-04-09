48b509ae9d630d3732b8cae4644e83e4603701ea
shlkn <pro100iliyash@yandex.ru>
2021-04-09 00:21:37 -0700

    "fix"

--- program2.c
+++ program2.c
@@ -6,34 +6,32 @@ void gamma_process(FILE *pfsf, FILE *pfgf, char *result_fl_name);// function tha
 
 int main()
 {
+	// handle pipe for source string
 	printf("Enter source file\n");
 	char srcflName[256];
 	scanf("%s", srcflName);
-	char program[] = "./program1 ", *cmd;
+	char program[256] = "./program1 ", *cmd;
 	cmd = strcat(program, srcflName);
 	FILE *pfsf; // pipe for source file
 	pfsf = popen(cmd, "r");
-	// handle pipe for source string
-
 
 	//taking gamma key from another file
 	printf("Enter file that contains gamma key\n");
 	char gamflName[256];
 	scanf("%s", gamflName);
-	cmd = strcat(program, gamflName);
+	char program2[256] = "./program1 ";
+	cmd = strcat(program2, gamflName);
 	FILE *pfgf; // pipe for gamma file
 	pfgf = popen(cmd, "r");
 
-
-
-
 	char result_fl_name[] = "result.txt";
-	char decrypted_fl_name[] = "resultdec.txt";
-	gamma_process(pfsf, pfgf, result_fl_name);
+	gamma_process(pfsf, pfgf, result_fl_name); // file encryption
 	// encrypted
-	FILE *result = fopen(result_fl_name, "r");
-	gamma_process(result, pfgf, decrypted_fl_name);
-	fclose(result);
+	/*char decrypted_fl_name[] = "resultdec.txt";
+	char program3[256] = "./program1 ";
+	cmd = strcat(program3, result_fl_name);
+	pfsf = popen(cmd, "r");
+	gamma_process(pfsf, pfgf, decrypted_fl_name);*/
 	//decrypted
 }
 
